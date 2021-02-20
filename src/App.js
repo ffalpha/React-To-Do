d59359768818c8769672c1e4cf82ee1d20fd1968
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-20 21:53:30 +0530

    No idea

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import Layout from "./layout/Layout";
+import Layout from "./hoc/layout/Layout";
 
 const App = () => {
   return <Layout>sa</Layout>;
--- src/hoc/layout/Layout.js
+++ src/hoc/layout/Layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../components/Navbar/Navbar";
+import Navbar from "../../components/Navigation/Navbar/Navbar";
 import styled from "styled-components";
 
 const MainWarpper = styled.div`
